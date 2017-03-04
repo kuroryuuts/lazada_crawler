c56cb5ed472b260b73ef113557ab0b352132d748
kuroryuuts <14520486@gm.uit.edu.vn>
2017-03-04 19:31:14 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinTrinhLibrary;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace lazada_crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Star Crawler....");
+            Console.OutputEncoding = Encoding.UTF8;
+            string fpath = "D:\\result.txt";//tạo file lưu trữ thông tin lấy được
+            FileStream fs = new FileStream(fpath, FileMode.Create);           
+            StreamWriter swriter = new StreamWriter(fs, Encoding.UTF8);
+            //GET : http://www.lazada.vn/
+            TinTrinhLibrary.WebClient client = new TinTrinhLibrary.WebClient(); //tạo kết nối
+            for (int i = 1; i <= 100; i++) //vòng lặp tăng số trang lên, tăng i theo số trang cần lấy
+            {
+                string html = client.Get("http://www.lazada.vn/phu-kien-thiet-bi-xe-mo-to-xe-dia-hinh/?page=" + i, "http://www.lazada.vn/", ""); //vào trang web về thiết bị xe mo to xe dia hinh
+                MatchCollection listLink = Regex.Matches(html, "<a href=\"/(.*?)\" class=\"c-product-card__name\">"); //danh sách link từng sản phẩm
+                foreach (Match link in listLink) //vòng lặp để vào từng sản phẩm
+                {
+                    string html1 = client.Get("http://www.lazada.vn/" + link.Groups[1].Value, "http://www.lazada.vn/", "");//tạo đường link đến từng sản phẩm
+                    Match motor_name = Regex.Match(html1, "title\" content=\"(.*?)\"/>"); //lấy tên sản phẩm
+                    Match motor_trademark = Regex.Match(html1, "'><span>(.*?)</span></a>"); //lấy thương hiệu sản phẩm
+                    Match motor_guarantee = Regex.Match(html1, "term\">(.*?)</span>", RegexOptions.Singleline); //lấy Bảo hành sản phẩm
+                    Match motor_price = Regex.Match(html1, "special_price_box\">(.*?)</span>", RegexOptions.Singleline); //lấy giá về sản phẩm
+                    Match motor_desc = Regex.Match(html1, "<li class=\"\"><span>(.*?)</span></li>", RegexOptions.Multiline); //lấy mô tả về sản phẩm
+                    Match motor_pprice = Regex.Match(html1, " id=\"price_box\">(.*?),</span>", RegexOptions.Singleline); //lấy giá gốc của sản phẩm
+                    Match motor_saving = Regex.Match(html1, "price_highlight\">(.*?)</span>", RegexOptions.Singleline); //lấy tiết kiệm của sản phẩm
+                    Match motor_number = Regex.Match(html1, "stock-number\">(.*?)</span>", RegexOptions.Singleline); //lấy số lượng sản phẩm còn lại
+                    Match motor_amortization = Regex.Match(html1, "\" rel=\".*?\">(.*?)»", RegexOptions.Singleline); //lấy hình thức trả góp sản phẩm
+                    swriter.WriteLine("Tên Phụ kiện - Xe máy: " + motor_name.Groups[1].Value.Trim()); //in ra 
+                    swriter.WriteLine("Thương hiệu: " + motor_trademark.Groups[1].Value.Trim());
+                    swriter.WriteLine("Mô tả: " + motor_desc.Groups[1].Value.Trim());
+                    swriter.WriteLine("Thông tin Bảo hành: " + motor_guarantee.Groups[1].Value.Trim());
+                    swriter.WriteLine("Giá: " + motor_price.Groups[1].Value.Trim(), "VND");
+                    swriter.WriteLine("Giá trước đây: " + motor_pprice.Groups[1].Value.Trim(), "VND");
+                    swriter.WriteLine("Tiết kiệm: " + motor_saving.Groups[1].Value.Trim());
+                    swriter.WriteLine("Số lượng: " + motor_number.Groups[1].Value.Trim());
+                    swriter.WriteLine("Trả Góp: " + motor_amortization.Groups[1].Value.Trim());
+                    swriter.WriteLine("\n====================================\n");
+                }
+            }
+            swriter.Flush();
+            fs.Close();
+            Console.WriteLine("Ok");
+            Console.ReadLine();
+        }
+    }
+}
+
+                
+    
---
+++ WebClient.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * Author: Tin Trinh
+ * General Web Client based HTTP protocol to submit Post/Get request to Web Server
+ * All rights reserved by Tin Trinh
+ */
+
+using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace TinTrinhLibrary
+{
+    public class WebClient
+    {
+        private CookieContainer cookieContainer = new CookieContainer(); //Keep cookies and feed cookies next request
+
+        public WebClient()
+        {
+            //Support HTTPS/SSL
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+        }
+
+        /// <summary>
+        /// Send GET HTTPRequest
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="referer"></param>
+        /// <param name="returnCookie">CookieName that you want to check</param>
+        /// <returns>If returnCookie is set, return cookie value. Else return html code</returns>
+        public String Get(string url, string referer, string returnCookie)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.AllowAutoRedirect = false;
+            webRequest.AutomaticDecompression = DecompressionMethods.GZip;
+
+            webRequest.CookieContainer = cookieContainer;
+
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0";
+            webRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            webRequest.Headers[HttpRequestHeader.AcceptLanguage] = "vi-VN,vi;q=0.8,en-US;q=0.5,en;q=0.3";
+            webRequest.Headers[HttpRequestHeader.AcceptEncoding] = "gzip, deflate, br";
+            webRequest.KeepAlive = false; //Fix "The server committed a protocol violation. Section=ResponseStatusLine"
+            webRequest.ContentType = "text/html; charset=UTF-8";
+
+            webRequest.Referer = referer;
+
+            webRequest.Method = "GET";
+
+            try
+            {
+                // get the response
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    if (webResponse == null)
+                    { return null; }
+
+                    if (returnCookie != "" && returnCookie != null)
+                    {
+                        //Developer can check cookies here at webResponse.Cookies
+                        foreach (Cookie cookie in webResponse.Cookies)
+                        {
+                            if (cookie.Name == returnCookie)
+                                return cookie.Value;
+                        }
+                        return null;
+                    }
+
+                    using (StreamReader sr = new StreamReader(webResponse.GetResponseStream()))
+                    {
+                        return sr.ReadToEnd().Trim();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                throw new Exception(GetInternalException(ex));
+            }
+            finally
+            {
+                webRequest.Abort();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Send POST HTTPRequest
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="parameters"></param>
+        /// <param name="referer"></param>
+        /// <param name="returnCookie">CookieName that you want to check</param>
+        /// returns>If returnCookie is set, return cookie value. Else return html code</returns>
+        public String Post(string url, string parameters, string referer, string returnCookie)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url); ;
+
+            webRequest.AllowAutoRedirect = false;
+            webRequest.AutomaticDecompression = DecompressionMethods.GZip;
+
+            webRequest.CookieContainer = cookieContainer;
+
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; rv:13.0) Gecko/20100101 Firefox/13.0.1";
+            webRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            webRequest.Headers[HttpRequestHeader.AcceptLanguage] = "en-us,en;q=0.5";
+            webRequest.Headers[HttpRequestHeader.AcceptEncoding] = "gzip,deflate";
+            webRequest.Headers[HttpRequestHeader.AcceptCharset] = "ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+            webRequest.KeepAlive = false; //Fix "The server committed a protocol violation. Section=ResponseStatusLine"
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            webRequest.Referer = referer;
+
+            webRequest.Method = "POST";
+            byte[] bytes = Encoding.UTF8.GetBytes(parameters);
+            Stream os = null;
+            try
+            { // send the Post
+                webRequest.ContentLength = bytes.Length; //Count bytes to send
+                os = webRequest.GetRequestStream();
+                os.Write(bytes, 0, bytes.Length); //Send it
+            }
+            catch (WebException ex)
+            {
+                throw new Exception(GetInternalException(ex));
+            }
+            finally
+            {
+                if (os != null)
+                {
+                    os.Close();
+                }
+            }
+
+            try
+            {
+                // get the response
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    if (webResponse == null)
+                    { return null; }
+
+                    if (returnCookie != "" && returnCookie != null)
+                    {
+                        //Developer can check cookies here at webResponse.Cookies
+                        foreach (Cookie ck in webResponse.Cookies)
+                        {
+                            if (ck.Name == returnCookie)
+                                return ck.Value;
+                        }
+                        return null;
+                    }
+
+                    StreamReader sr = new StreamReader(webResponse.GetResponseStream());
+                    return sr.ReadToEnd().Trim();
+                }
+            }
+            catch (WebException ex)
+            {
+                throw new Exception(GetInternalException(ex));
+            }
+            finally
+            {
+                webRequest.Abort();
+            }
+            return null;
+        }
+
+        public string GetInternalException(WebException ex)
+        {
+            StreamReader sr = new StreamReader(ex.Response.GetResponseStream());
+            string errorMessage = sr.ReadToEnd().Trim();
+            return errorMessage;
+        }
+    }
+}
